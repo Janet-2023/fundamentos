fb1a70e1840924dc8836dd342178202aac749d55
Janet <nope.janet.97@gmail.com>
2023-04-24 22:13:24 -0500

    agregando

---
+++ app.js
@@ -0,0 +1,44 @@
+//Creacion de variables
+// var , const y let 
+
+// TIPOS DE DATOS EN JS
+
+/* 
+1. STREAM
+2. Number
+3. Boolean
+4. Null
+5. Undefined
+6. object
+7. Array
+*/
+let name = 'Janet';
+let lastname= 'Ñope';
+let gender = 'F';
+let country = 'Perú';
+
+let age = 27;
+let year = 1997;
+let date = 24/10;
+
+let logged= true;
+let notlogged = false;
+let active=true;
+let inactive=false;
+
+
+console.log(typeof name);
+console.log(typeof lastname);
+console.log(typeof country);
+
+
+console.log(typeof age);
+console.log(typeof year);
+console.log(typeof date);
+
+console.log(typeof logged);
+console.log(typeof notlogged);
+console.log(typeof active);
+console.log(typeof inactive);
+
+
